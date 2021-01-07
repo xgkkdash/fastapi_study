b1983ff88ac80b0beb80a2d684987655e356c447
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-01-08 02:11:29 +0800

    update frontend_app to use mongodb

--- frontend_app/main.py
+++ frontend_app/main.py
@@ -1,35 +1,32 @@
+import os
 import requests
 from typing import Optional
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
 
-from crud import crud
-from models import models
+from models.kvpair import Kvpair
 from schemas import schemas
-from database.database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
+from database.mongodb import MongoDatabase
 
 app = FastAPI()
 
 
 # Dependency
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    db_name = os.environ.get('DB_NAME', 'front_dev')
+    db_host = os.environ.get('DB_HOST', 'localhost')
+    db_port = os.environ.get('DB_PORT', '27017')
+    db_url = "mongodb://" + db_host + ":" + str(db_port) + "/"
+    db = MongoDatabase(db_name, db_url)
+    yield db
 
 
 @app.post("/kvpairs/", response_model=schemas.KvpairDetail)
-def create_kvpair(kvpair_detail: schemas.KvpairDetailCreate, db: Session = Depends(get_db)):
-    db_kvpair = crud.get_kvpair(db, key=kvpair_detail.key)
+def create_kvpair(kvpair_detail: schemas.KvpairDetailCreate, db=Depends(get_db)):
+    db_kvpair = db.get_kvpair(key=kvpair_detail.key)
     if db_kvpair:
         raise HTTPException(status_code=400, detail="key already existed")
     else:
-        kvpair = schemas.KvpairCreate(key=kvpair_detail.key, value=kvpair_detail.value)
         if kvpair_detail.id:
             back_data = None
             backend_url = "http://backend:5000/details/"
@@ -41,20 +38,22 @@ def create_kvpair(kvpair_detail: schemas.KvpairDetailCreate, db: Session = Depen
             except Exception as e:
                 print(e)
             if back_data:
-                front_result = crud.create_kvpair(db, kvpair=kvpair)
+                front_result = Kvpair(kvpair_detail.key, kvpair_detail.value)
+                db.save_kvpair(front_result)
                 final_result = schemas.KvpairDetail(key=front_result.key, value=front_result.value,
                                                     id=back_data['id'], description=back_data['description'])
                 return final_result
             else:
                 raise HTTPException(status_code=400, detail="insert id/description failed")
         else:
-            front_result = crud.create_kvpair(db, kvpair=kvpair)
+            front_result = Kvpair(kvpair_detail.key, kvpair_detail.value)
+            db.save_kvpair(front_result)
             final_result = schemas.KvpairDetail(key=front_result.key, value=front_result.value)
             return final_result
 
 
 @app.get("/kvpairs/{key}", response_model=schemas.KvpairDetail)
-def read_kvpair(key: str, id: Optional[int] = 0,  db: Session = Depends(get_db)):
+def read_kvpair(key: str, id: Optional[int] = 0,  db=Depends(get_db)):
     back_data = None
     if id:
         backend_url = "http://backend:5000/details/" + str(id)
@@ -64,7 +63,7 @@ def read_kvpair(key: str, id: Optional[int] = 0,  db: Session = Depends(get_db))
                 back_data = backend_response.json()
         except Exception as e:
             print(e)
-    db_kvpair = crud.get_kvpair(db, key=key)
+    db_kvpair = db.get_kvpair(key=key)
     if db_kvpair is None:
         raise HTTPException(status_code=404, detail="Key not found")
     else:
---
+++ frontend_app/models/kvpair.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Kvpair:
+    key: str
+    value: str
