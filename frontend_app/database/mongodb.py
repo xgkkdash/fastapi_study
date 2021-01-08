577d0add0912dbd73c9c5b10aabbfe42485bb578
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-01-09 03:17:45 +0800

    frontend_app use motor and aiohttp to make async more clear

--- frontend_app/database/mongodb.py
+++ frontend_app/database/mongodb.py
@@ -1,36 +1,18 @@
-import mongoengine
-from mongoengine import DoesNotExist
-
-from database.KvpairDocument import KvpairDocument
+import asyncio
+import motor
 
 
-class MongoDatabase:
-    def __init__(self, db_name, db_url):
-        self.db_connection = mongoengine.connect(db=db_name, host=db_url)
-        self.db_name = db_name
+async def insert_kvpair(db, kvpair):
+    doc = await db.kvpairs.insert_one(dict(kvpair.__dict__))
+    return doc
 
-    def save_kvpair(self, kvpair):
-        doc = KvpairDocument.from_kvpair(kvpair)
-        return doc.save()
 
-    def get_kvpair(self, key):
-        try:
-            doc = KvpairDocument.objects.get(key=key)
-            return doc.to_kvpair()
-        except DoesNotExist:
-            return None
+async def find_kvpair(db, key):
+    doc = await db.kvpairs.find_one({"key": key})
+    return doc if doc else None
 
-    def remove_kvpair(self, **kwargs):
-        doc = KvpairDocument.objects.get(**kwargs)
-        doc.delete()
 
-    def update_kvpair(self, key, new_value):
-        try:
-            doc = KvpairDocument.objects.get(key=key)
-            return doc.update(value=new_value)
-        except DoesNotExist:
-            return None
+async def drop_kvpairs(db):
+    result = await db.kvpairs.drop()
+    return result
 
-    # Warning: this method should be used carefully and only in the test environment
-    def drop_database(self):
-        self.db_connection.drop_database(self.db_name)
--- frontend_app/main.py
+++ frontend_app/main.py
@@ -1,14 +1,14 @@
 import asyncio
-import functools
+import aiohttp
+import motor.motor_asyncio
 import os
-import requests
 from typing import Optional
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 
 from models.kvpair import Kvpair
 from schemas import schemas
-from database.mongodb import MongoDatabase
+from database.mongodb import find_kvpair, insert_kvpair
 
 app = FastAPI()
 
@@ -19,31 +19,30 @@ def get_db():
     db_host = os.environ.get('DB_HOST', 'localhost')
     db_port = os.environ.get('DB_PORT', '27017')
     db_url = "mongodb://" + db_host + ":" + str(db_port) + "/"
-    db = MongoDatabase(db_name, db_url)
-    yield db
+    db_client = motor.motor_asyncio.AsyncIOMotorClient(db_url)
+    return db_client[db_name]
+
+
+db = get_db()
 
 
 @app.post("/kvpairs/", response_model=schemas.KvpairDetail)
-async def create_kvpair(kvpair_detail: schemas.KvpairDetailCreate, db=Depends(get_db)):
-    loop = asyncio.get_event_loop()
-    db_kvpair = await loop.run_in_executor(None, db.get_kvpair, kvpair_detail.key)
-    if db_kvpair:
+async def create_kvpair(kvpair_detail: schemas.KvpairDetailCreate):
+    db_kvpair_doc = await find_kvpair(db, kvpair_detail.key)
+    if db_kvpair_doc:
         raise HTTPException(status_code=400, detail="key already existed")
     else:
         if kvpair_detail.id:
             back_data = None
             backend_url = "http://backend:5000/details/"
-            try:
-                param_json = {"id": kvpair_detail.id, "description": kvpair_detail.description}
-                backend_response = await loop.run_in_executor(None, functools.partial(requests.post, url=backend_url,
-                                                                                      json=param_json))
-                if backend_response.status_code == 200:
-                    back_data = backend_response.json()
-            except Exception as e:
-                print(e)
+            param_json = {"id": kvpair_detail.id, "description": kvpair_detail.description}
+            async with aiohttp.ClientSession() as session:
+                async with session.post(backend_url, json=param_json) as response:
+                    if response.status == 200:
+                        back_data = await response.json()
             if back_data:
                 front_result = Kvpair(kvpair_detail.key, kvpair_detail.value)
-                loop.run_in_executor(None, db.save_kvpair, front_result)
+                await insert_kvpair(db, front_result)
                 final_result = schemas.KvpairDetail(key=front_result.key, value=front_result.value,
                                                     id=back_data['id'], description=back_data['description'])
                 return final_result
@@ -51,28 +50,27 @@ async def create_kvpair(kvpair_detail: schemas.KvpairDetailCreate, db=Depends(ge
                 raise HTTPException(status_code=400, detail="insert id/description failed")
         else:
             front_result = Kvpair(kvpair_detail.key, kvpair_detail.value)
-            loop.run_in_executor(None, db.save_kvpair, front_result)
+            await insert_kvpair(db, front_result)
             final_result = schemas.KvpairDetail(key=front_result.key, value=front_result.value)
             return final_result
 
 
 @app.get("/kvpairs/{key}", response_model=schemas.KvpairDetail)
-async def read_kvpair(key: str, id: Optional[int] = 0,  db=Depends(get_db)):
-    loop = asyncio.get_event_loop()
+async def read_kvpair(key: str, id: Optional[int] = 0):
     # TODO: use asyncio.tasks instead of run_in_executor to concurrently get db_kvpair and back_data
     back_data = None
     if id:
         backend_url = "http://backend:5000/details/" + str(id)
-        try:
-            backend_response = await loop.run_in_executor(None, requests.get, backend_url)
-            if backend_response.status_code == 200:
-                back_data = backend_response.json()
-        except Exception as e:
-            print(e)
-    db_kvpair = await loop.run_in_executor(None, db.get_kvpair, key)
-    if db_kvpair is None:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(backend_url) as response:
+                if response.status == 200:
+                    back_data = await response.json()
+    db_kvpair_doc = await find_kvpair(db, key)
+    if db_kvpair_doc is None:
         raise HTTPException(status_code=404, detail="Key not found")
     else:
+        db_kvpair_doc.pop('_id')
+        db_kvpair = Kvpair(**db_kvpair_doc)
         final_result = schemas.KvpairDetail(key=db_kvpair.key, value=db_kvpair.value)
         if back_data:
             final_result.id = back_data['id']
